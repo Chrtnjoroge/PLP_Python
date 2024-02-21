6bd4ea056f2fb7778f3965e5eb105c282ff52e92
Charity Njoroge <123123079+Chrtnjoroge@users.noreply.github.com>
2024-02-21 12:07:47 +0300

    Add files via upload

---
+++ Week1.py
@@ -0,0 +1,11 @@
+# Ask the user to enter their name
+name = input("What is your name? ")
+
+# Ask the user to input their age
+age = input("How old are you? ")
+
+# Ask user to input their location
+location = input("Where do you live? ")
+
+# output message using the user's name, age, and location
+print("Hey {}, you are {} years old and live in {}.".format(name, age, location))
